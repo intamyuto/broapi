63510f64c1c2e505ab87ec9a6113b63f18caf883
Vsevolod Okhrin <intamyuto@gmail.com>
2024-10-23 14:58:30 +0300

    allow option to disable notifications

--- app/dependencies.py
+++ app/dependencies.py
@@ -20,13 +20,22 @@ async def get_session() -> AsyncSession: # type: ignore ¯\_(ツ)_/¯
         yield session
 
 
-bot_token = os.environ['BROAPI_BOT_TOKEN']
+bot_token = os.getenv('BROAPI_BOT_TOKEN')
 
-bot = Bot(token=bot_token)
+bot: Bot = None
+if bot_token:
+    bot = Bot(token=bot_token)
+
+if bot is None:
+    print("bot disabled")
 
 notifications_whitelist = [240329934, 862139934, 876628085, 860108961, 876241289, 1055487318, 209247857, 1904172074, 5119664278, 6083350394, 7034617135, 624161982, 779238503, 181088439]
 
 async def send_notifications(user_id: int, message: str):
+    if bot is None:
+        print("skip notification; bot disabled")
+        return
+
     # if notifications_whitelist and user_id in notifications_whitelist:
     try:
         message = await bot.send_message(chat_id=user_id, text=message, reply_markup=bro_button())
